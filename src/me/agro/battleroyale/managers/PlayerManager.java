199ec263a99371dbce5389c0a4982bbf4dd8b661
Nicholas Abbate <41085045+AgroUberDriver@users.noreply.github.com>
2021-11-08 14:23:53 -0500

    Completed Scatter Function.

--- src/me/agro/battleroyale/managers/PlayerManager.java
+++ src/me/agro/battleroyale/managers/PlayerManager.java
@@ -1,5 +1,12 @@
 package me.agro.battleroyale.managers;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.Random;
+
 public class PlayerManager {
 
     private GameManager gameManager;
@@ -11,6 +18,22 @@ public class PlayerManager {
 
 
     public void scatterPlayers() {
+
+        Random r = new Random();
+        int Low = 50;
+        int High = 200;
+
+
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            World world = p.getWorld();
+            int x = r.nextInt(High-Low) + Low;
+            int z = r.nextInt(High-Low) + Low;
+            int y = world.getHighestBlockYAt(x,z);
+
+            Location location = new Location(p.getWorld(), x,y,z);
+            p.teleport(location);
+
+        }
 
     }
 }
